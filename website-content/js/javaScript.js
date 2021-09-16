0b1e56bfcf7150c42aca2177beb3247be4612882
Malin Liedholm <malin.liedholm@gmail.com>
2021-09-16 12:33:44 +0200

    added basic website layout with menu

---
+++ website-content/js/javaScript.js
@@ -0,0 +1,10 @@
+function clickNavMenu() {
+  var smallNavBar = document.getElementById("small-nav-bar");
+    
+  if(smallNavBar.className.indexOf("w3-show") != -1) {
+      smallNavBar.className = smallNavBar.className.replace(" w3-show", "");
+  }
+  else {
+      smallNavBar.className += " w3-show";
+  }
+}
