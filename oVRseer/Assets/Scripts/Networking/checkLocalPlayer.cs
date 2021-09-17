a69335fda6d03733b06e20730ecaa75ed053661d
Maximillianmus <max.truedsson1998@gmail.com>
2021-09-18 00:31:41 +0200

    Changes to the prefabs that we spawn, aswell as generall XR controll changes

--- oVRseer/Assets/Scripts/Networking/checkLocalPlayer.cs
+++ oVRseer/Assets/Scripts/Networking/checkLocalPlayer.cs
@@ -2,35 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using UnityEngine.XR.Management;
+using UnityEngine.SpatialTracking;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
 
-public class checkLocalPlayer : MonoBehaviour
+public class checkLocalPlayer : NetworkBehaviour
 {
-    public Transform[] disabledTransforms;
-    public Component[] disabledComponents;
-    
+    //enable these transforms
+    public Transform[] EnableTransforms;
+    public bool isVr;
+
+    [Header("leave empty if not VR ")]
+    [SerializeField] XRRig XR_Rig;
+    [SerializeField] ActionBasedController ActionControllerLeft;
+    [SerializeField] XRRayInteractor XRRayInteractorLeft;
+    [SerializeField] ActionBasedController ActionControllerRight;
+    [SerializeField] XRRayInteractor XRRayInteractorRight;
+    [SerializeField] InputActionManager inputActionManager;
 
 
 
-    // Start is called before the first frame update
-    void Start()
+    //this only runes if the object it is on is the local player, so we enable all the controlls and cammeras here
+    public override void OnStartLocalPlayer()
     {
-        if(gameObject != NetworkClient.localPlayer.gameObject)
+
+        NetworkIdentity netID = GetComponent<NetworkIdentity>();
+
+        Debug.LogWarning("The code in the check loader is currently using a hack to circumvent a bug, please fix bug and then remove circumvention. Bug can be found on git");
+        if (netID.isLocalPlayer && netID.netId != 3 && netID.netId != 4)
         {
-            for (int i = 0; i < disabledTransforms.Length; i++)
+            for (int i = 0; i < EnableTransforms.Length; i++)
             {
-                Transform.Destroy(disabledTransforms[i].gameObject);
+                EnableTransforms[i].gameObject.SetActive(true);
             }
 
-            for (int i = 0; i < disabledTransforms.Length; i++)
+            if (!isVr)
             {
-                print(disabledComponents.Length);
-                print(i);
-                Component.Destroy(disabledComponents[i]);
+                //removes the VR settings if the local player is not a vr player
+                XRGeneralSettings.Instance.Manager.DeinitializeLoader();
+                print("Turning of Vr");
+                print(GetComponent<NetworkIdentity>().netId);
+                print(gameObject.name);
             }
-
-
+            else
+            {
+                print("initializing VR");
+                XRGeneralSettings.Instance.Manager.InitializeLoader();
+                XR_Rig.enabled = true;
+                ActionControllerLeft.enabled = true;
+                XRRayInteractorLeft.enabled = true;
+                ActionControllerRight.enabled = true;
+                XRRayInteractorRight.enabled = true;
+                inputActionManager.enabled = true;
+            }
         }
-
+       
+        
     }
 
 }
