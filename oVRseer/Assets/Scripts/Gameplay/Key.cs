86c9ff6d9cd53d98d5266e50160076f0934ecabf
Malin Liedholm <malin.liedholm@gmail.com>
2021-09-21 18:26:07 +0200

    Added function to create and place keys

--- oVRseer/Assets/Scripts/Gameplay/Key.cs
+++ oVRseer/Assets/Scripts/Gameplay/Key.cs
@@ -5,47 +5,17 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
-    //public Text keyCollectedMessage;
-    // bool textFadeIn = false;
-
-    //private float textFadeTime = 1.5f;
     public GameObject keyHandler;
 
-
+    // Key is collected
     private void OnTriggerEnter(Collider other) {
-        // Key is collected, TODO: need to update UI for all tiny-guy, send message to keyhandler, delet key. 
-        keyHandler.GetComponent<KeyHandler>().textFadeIn = true;
+
+        // Update UI and number of keys in key handler
+        keyHandler.GetComponent<KeyHandler>().FadeInText();
         keyHandler.GetComponent<KeyHandler>().KeyCollected();
-    }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //keyCollectedMessage.canvasRenderer.SetAlpha(0);
-        //keyCollectedMessage.text = "A key has been collected";
+        // Delete "key"
+        // TODO: Might want to add some cool effect for when this object is collected
+        gameObject.SetActive(false);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        /*
-        if (textFadeIn == true)
-        {
-            //Fully fade in Image (1) with the duration of 2
-            keyCollectedMessage.CrossFadeAlpha(1, textFadeTime, false);
-            Invoke("FadeOutText", textFadeTime + 1);
-        }
-
-        if (textFadeIn == false)
-        {
-            keyCollectedMessage.CrossFadeAlpha(0, 1.0f, false);
-        }
-        */
-    }
-    /*
-    private void FadeOutText()
-    {
-        textFadeIn = false;
-    }
-    */
 }
--- oVRseer/Assets/Scripts/Gameplay/KeyHandler.cs
+++ oVRseer/Assets/Scripts/Gameplay/KeyHandler.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 
 public class KeyHandler : MonoBehaviour
 {
-    public int numberOfKeys = 4;
-    private bool doorIsLocked;
-    private float textFadeTime = 1.5f;
+    public int numberOfKeys = 1;
     public Text keyCollectedMessage;
-    public bool textFadeIn;
+    public Vector3[] keyPositions;
+
+    private int maxNumberOfKeys = 4;
+    private bool doorIsLocked;
+    private float textFadeTime = 1f;
+    private bool textFadeIn;
     private int keysRemaining;
 
+    public GameObject keyPrefab;
+    public GameObject theKeyHandler;
 
     // Start is called before the first frame update
     void Start()
@@ -20,39 +25,77 @@ public class KeyHandler : MonoBehaviour
         textFadeIn = false;
         doorIsLocked = true;
         keyCollectedMessage.canvasRenderer.SetAlpha(0);
-        
+
+        if(numberOfKeys > maxNumberOfKeys) {
+            numberOfKeys = maxNumberOfKeys;
+        }
+
+        FillPositions();
+        CreateKeys();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (textFadeIn == true)
-        {
-            //Fully fade in Image (1) with the duration of 2
+        TextFade();
+    }
+
+    private void TextFade()
+    {
+        if (textFadeIn == true) {
+            //Fully fade in text with duration of "textFadeTime" 
             keyCollectedMessage.CrossFadeAlpha(1, textFadeTime, false);
             Invoke("FadeOutText", textFadeTime + 1);
-            
+
         }
 
-        if (textFadeIn == false)
-        {
+        if (textFadeIn == false){
             keyCollectedMessage.CrossFadeAlpha(0, 1.0f, false);
         }
     }
 
-    private void FadeOutText()
+    public void FadeOutText()
     {
         textFadeIn = false;
+    }
+
+    public void FadeInText()
+    {
+        textFadeIn = true;
     }
 
     public void KeyCollected()
     {
         keysRemaining -= 1;
-        keyCollectedMessage.text = "A key has been collected" + "\n" + keysRemaining + " keys remaining";
 
-        if(keysRemaining <= 0)
+        if(keysRemaining > 0)
+        {
+            keyCollectedMessage.text = "A key has been collected" + "\n" + keysRemaining + " keys remaining";
+        }
+        else
         {
             doorIsLocked = false;
+            keyCollectedMessage.text = "The door is unlocked. Go to it to escape";
+        }
+    }
+
+    private void FillPositions()
+    {
+        // Hard coded positions for now
+        keyPositions = new Vector3[4];
+        keyPositions[0] = new Vector3(0f, 5f, 0f);
+        keyPositions[1] = new Vector3(0f, 5f, 3f);
+        keyPositions[2] = new Vector3(0f, 5f, 6f);
+        keyPositions[3] = new Vector3(0f, 5f, 9f);
+    }
+
+    private void CreateKeys() {
+
+        for(int i = 0; i < numberOfKeys; i++)
+        {
+            GameObject temp = Instantiate(keyPrefab, keyPositions[i], Quaternion.identity) as GameObject;
+            temp.name = "Key" + i.ToString();
+            temp.GetComponent<Key>().keyHandler = theKeyHandler;
         }
     }
 }
