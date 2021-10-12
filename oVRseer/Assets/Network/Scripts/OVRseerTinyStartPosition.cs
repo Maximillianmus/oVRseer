cf919914938152e7216db2a4e24678fcb5c9fd7d
Robin B <robin.bertin@grenoble-inp.org>
2021-10-12 15:52:51 +0200

    UI instruction & Change DIR

---
+++ oVRseer/Assets/Network/Scripts/OVRseerTinyStartPosition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Network
+{
+    public class OVRseerTinyStartPosition : MonoBehaviour
+    {
+        private void Awake()
+        {
+            PlayerSpawnSystem.AddTinySpawnPoint(transform);
+        }
+
+        private void OnDestroy()
+        {
+            PlayerSpawnSystem.AddTinySpawnPoint(transform);
+        }
+    }
+}
