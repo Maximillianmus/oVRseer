ee1be3c64c6ecd33d8a8dfe02be537bc083a5322
Max Truedsson <32105590+Maximillianmus@users.noreply.github.com>
2021-10-21 19:26:54 +0200

    Hello

---
+++ oVRseer/Assets/Props/InitializeInteractionObject.cs
@@ -0,0 +1,41 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+
+//this component has the objective of giving the authority of this object to the VR player and then turning itself off
+public class InitializeInteractionObject : NetworkBehaviour
+{
+    bool foundVR = false;
+    GameObject VrPlayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        VrPlayer = GameObject.FindGameObjectWithTag("VR");
+
+        if (VrPlayer != null){
+            CmdGiveAuthority();
+            foundVR = true;
+            gameObject.GetComponent<XRGrabInteractable>().interactionManager = VrPlayer.GetComponentInChildren<XRInteractionManager>();
+        }
+
+        if (foundVR)
+        {
+            this.enabled = false;
+        }
+
+    }
+
+    [Command(requiresAuthority = false)]
+    void CmdGiveAuthority()
+    {
+        print("authority changed");
+        netIdentity.RemoveClientAuthority();
+        netIdentity.AssignClientAuthority(VrPlayer.GetComponent<NetworkIdentity>().connectionToClient);
+    }
+}
