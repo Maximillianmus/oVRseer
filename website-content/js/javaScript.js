e9d0d18bd2f9434f3298156153af8909d15d4b10
Malin Liedholm <malin.liedholm@gmail.com>
2021-10-28 19:27:21 +0200

    more progress - image slide gallery

--- website-content/js/javaScript.js
+++ website-content/js/javaScript.js
@@ -8,3 +8,36 @@ function clickSmallNavMenu() {
       smallNavBar.className += " w3-show";
   }
 }
+
+// Gallery slides
+var currentImage = 1;
+showImage(currentImage);
+
+function plusDivs(n) {
+  showImage(currentImage += n);
+}
+
+function currentDiv(n) {
+  showImage(currentImage = n);
+}
+
+function showImage(n) {
+  var galleryImage = document.getElementsByClassName("galleryImageSlides");
+    
+  //var dots = document.getElementsByClassName("demo");
+  if (n > galleryImage.length) {currentImage = 1}
+  
+  if (n < 1) {currentImage = galleryImage.length}
+  
+  var i;
+  for (i = 0; i < galleryImage.length; i++) {
+    galleryImage[i].style.display = "none";  
+  }
+    
+  //for (i = 0; i < dots.length; i++) {
+  //dots[i].className = dots[i].className.replace(" w3-white", "");
+  //}
+    
+  galleryImage[currentImage-1].style.display = "block";  
+  //dots[currentImage-1].className += " w3-white";
+}
