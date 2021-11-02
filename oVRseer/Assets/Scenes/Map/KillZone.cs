b01ae0f96b9548e8ea016e52127c60f2c3001240
Robin B <robin.bertin@grenoble-inp.org>
2021-11-02 16:23:37 +0100

    Change KillZone Trigger

--- oVRseer/Assets/Scenes/Map/KillZone.cs
+++ oVRseer/Assets/Scenes/Map/KillZone.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class KillZone : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.tag == "PlayerArmature")
         {
