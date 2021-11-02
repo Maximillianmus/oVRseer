49ac345eb21cfe266e9864866ceff34a2b4f18cd
Robin B <robin.bertin@grenoble-inp.org>
2021-11-02 17:07:59 +0100

    Fix trigger with throwing

--- oVRseer/Assets/Scenes/Map/KillZone.cs
+++ oVRseer/Assets/Scenes/Map/KillZone.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 
 public class KillZone : MonoBehaviour
 {
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "PlayerArmature")
         {
-            if (!other.transform.root.GetComponent<NetworkIdentity>().hasAuthority)
-                return;
             other.transform.root.GetComponent<State>().Squashed();
         }
     }
